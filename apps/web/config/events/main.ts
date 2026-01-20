b2c20a17b3b0ae993371f395154e528ab6130ac8
omdash315 <omdash315@gmail.com>
2026-01-20 18:23:37 +0530

    Url issues and backticks fixed

--- apps/web/config/events/main.ts
+++ apps/web/config/events/main.ts
@@ -3,92 +3,92 @@ export const MainEvents = [
     club: `AXIOM`,
     rulebook: `https://drive.google.com/open?id=1N4X9oEpmyVI6xvA10uCsQmO_ualjYl8y`,
     name: `The Quest of Chromara`,
-    description: `"The Quest of Chromara is a treasure hunt where every clue leads you
-    closer to victory! Compete solo or in teams of up to three, scan QR codes at each checkpoint, crack clever clues, and race across locations. Think fast, move faster. The quickest team to finish wins it all!"`,
+    description: `The Quest of Chromara is a treasure hunt where every clue leads you
+    closer to victory! Compete solo or in teams of up to three, scan QR codes at each checkpoint, crack clever clues, and race across locations. Think fast, move faster. The quickest team to finish wins it all!`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909410/nitrutsav-2026/d3lg7vqgegedsafl5est.webp`,
   },
   {
     club: `Kalaam`,
-    rulebook: `https://drive.google.com/open?id=11FbRfaTaCv8_sjP-rhxkrL2tPAfMooXS`,
+    rulebook: `https://drive.google.com/open?id=178j8yGqA2d4SgLcVr5DqzjRIrTYKZFtY`,
     name: `KAARVAAN 5.0`,
-    description: `"Kaarvaan 5.0 is the flagship poetry event of NITRUTSAV 2026, presented exclusively by the
+    description: `Kaarvaan 5.0 is the flagship poetry event of NITRUTSAV 2026, presented exclusively by the
     members of the Kalaam Club. The event features a curated series of original spoken-poetry
     performances by the senior poets of the club. The performances will be in Hindi, Urdu
     and English and based on a wide variety of themes. We will be having our own hosts. The
     duration of all the performances (club members), including the hosting, will be approximately
-    1 hour 30 minutes."`,
+    1 hour 30 minutes.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909404/nitrutsav-2026/cbsnwcyblatkxaa6b9bs.webp`,
   },
   {
     club: `Kalaam`,
-    rulebook: `ttps://www.drive.comhttps://drive.google.com/open?id=1EVPJfMIF764kIRP0rB0eIOVGzOVNyVc0`,
+    rulebook: `https://drive.google.com/open?id=11FbRfaTaCv8_sjP-rhxkrL2tPAfMooXS`,
     name: `Just A Minute (JAM)`,
-    description: `"Just a Minute (JAM) is a public speaking game where parƟcipants must speak on a
+    description: `Just a Minute (JAM) is a public speaking game where parƟcipants must speak on a
 random topic for sixty seconds. The challenge is to talk without hesitaƟon, repeƟƟon,
 or deviaƟon. It tests mental agility, vocabulary, and composure, as opponents can
-""challenge"" to steal the remaining Ɵme."`,
+""challenge"" to steal the remaining Ɵme.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909403/nitrutsav-2026/b3zphnvpm9cb4h7cyiya.webp`,
   },
   {
     club: `OSS`,
     rulebook: `https://drive.google.com/open?id=1EVPJfMIF764kIRP0rB0eIOVGzOVNyVc0`,
     name: `ODIA HERITAGE TREASURE HUNT`,
-    description: `"The Odia Heritage Treasure Hunt is an indoor adventure across
+    description: `The Odia Heritage Treasure Hunt is an indoor adventure across
 selected LA rooms, where teams explore Odisha’s rich culture. By
 solving clues based on Odia history, traditions, art, festivals,
 cuisine, and personalities, participants unlock checkpoints. The
-first team to finish all stages wins."`,
+first team to finish all stages wins.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909408/nitrutsav-2026/xatrdzmmbiwwdqwps2er.webp`,
   },
   {
     club: `OSS`,
     rulebook: `https://drive.google.com/open?id=1NeatBU44cTDPIiS6CMHZpNkDTM5CsuSX`,
     name: `JHOTI FIESTA`,
-    description: `"The Jhoti Fiesta celebrates Odisha’s traditional floor art,
+    description: `The Jhoti Fiesta celebrates Odisha’s traditional floor art,
 showcasing intricate designs made with rice paste. Participants
 express creativity through cultural motifs, symmetry, and
 precision. The event promotes heritage, artistic skills, and
 appreciation of indigenous art forms while encouraging
-originality and aesthetic excellence."`,
+originality and aesthetic excellence.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909402/nitrutsav-2026/e3auuecgsbiob7p0fvrz.webp`,
   },
   {
     club: `Fusion`,
     rulebook: `https://drive.google.com/open?id=1LCr3bSW4x7RLAMXsiJ37byr3vCGw9BaQ`,
     name: `Midnight Masque`,
-    description: `"About the Event : A signature Day 1 formal masquerade night
+    description: `About the Event : A signature Day 1 formal masquerade night
 open to all students. Step into a mystical ballroom filled with
 fairy lights and fog for a night of mystery and elegance.
-Features high-energy EDM sets, themed photo booths"`,
+Features high-energy EDM sets, themed photo booths`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909409/nitrutsav-2026/e4hjaggzcrnvaesryw8c.webp`,
   },
   {
     club: `Fusion`,
-    rulebook: `https://www.drive.com`,
+    rulebook: `https://drive.google.com/open?id=15qR2v0eKBUd6nKxY3VkvbDPCh1BeZQoA`,
     name: `Street Vogue`,
-    description: `"Event: A high -energy, open-air fashion and personality
+    description: `Event: A high -energy, open-air fashion and personality
 contest held at LA Lawns. Kicking off with a flashmob, this
 Roadies-style event features raw auditions, spontaneous dares,
 and a pageant walk battle to crown the ultimate Campus Diva
-and Campus Hunk."`,
+and Campus Hunk.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909409/nitrutsav-2026/n2whjs3fbll6pxzlefqg.webp`,
   },
   {
     club: `Rainbow Dot`,
     rulebook: `https://drive.google.com/open?id=1mjTcPSIEVoeMzL3ia0NlKbSUx-Zcr8s4`,
     name: `Colour Carnival Parade`,
-    description: `"Colour Carnival Parade is a vibrant, open-for-all creative celebration featuring face painting,
+    description: `Colour Carnival Parade is a vibrant, open-for-all creative celebration featuring face painting,
 cupcake making, journaling, and canvas painting, concluding with a joyful carnival-style parade.
 The event promotes inclusivity, self-expression, and community bonding through colours, art, and
-shared experiences."`,
+shared experiences.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909413/nitrutsav-2026/qaewnwh5erb5snlgh1n3.webp`,
   },
   {
     club: `Hourglass`,
     rulebook: `https://drive.google.com/file/d/1VtNy601m2ujxlKYj9RDsmUo2IbkVfFtn/view?usp=sharing`,
     name: `Unpopular Opinion Courtroom`,
-    description: `"In this entertaining and humorous speaking activity, participants enter a mock courtroom where
-they must confidently defend a strange, funny, or highly unpopular opinion."`,
+    description: `In this entertaining and humorous speaking activity, participants enter a mock courtroom where
+they must confidently defend a strange, funny, or highly unpopular opinion.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768911213/nitrutsav-2026/q3egehmjzavzlj9bk8z4.webp`,
   },
   {
@@ -102,77 +102,77 @@ they must confidently defend a strange, funny, or highly unpopular opinion."`,
     club: `THIRD EYE`,
     rulebook: `https://drive.google.com/open?id=1wlsYhJ-xlqMHL8DAmtEl8piKeGC1VKpC`,
     name: `FRAME BY FRAME`,
-    description: `"Frame by Frame is a creative
+    description: `Frame by Frame is a creative
 photography competition that challenges participants to narrate a
 compelling story through a sequence of photographs. Open to
 photography enthusiasts across colleges, the event celebrates visual
 storytelling, creativity, and perspective through powerful frames
-captured over a limited time."`,
+captured over a limited time.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909411/nitrutsav-2026/jjcwurvwhggxkbhuw3sa.webp`,
   },
   {
     club: `Rotaract`,
     rulebook: `https://drive.google.com/open?id=1HpPc5bItz9Zmue8ZzlzXM9qo6-PGXaIe`,
     name: `DecryptX`,
-    description: `"DecryptX is a checkpoint-based
+    description: `DecryptX is a checkpoint-based
 team game where participants solve tasks, unlock coded boxes, and
 decode riddles through logical thinking, teamwork, and real-world
 kindness challenges. The event promotes problem-solving skills,
-discipline, and social responsibility in a fun, engaging format."`,
+discipline, and social responsibility in a fun, engaging format.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909405/nitrutsav-2026/twshe73jbb1flhrciqt1.webp`,
   },
   {
     club: `SDG Campus Club`,
     rulebook: `https://drive.google.com/open?id=1M8Kf19LGPrsDADm0pXgT5cxj_SqmC66-`,
     name: `Silent Sabotage`,
-    description: `"An Among Us–inspired city-building game where players vote on
+    description: `An Among Us–inspired city-building game where players vote on
 development policies while hidden imposters work to secretly sabotage the
 city. With bluffing, debate, and strategic decision-making, players must
-identify traitors and keep the city alive before everything falls apart."`,
+identify traitors and keep the city alive before everything falls apart.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909406/nitrutsav-2026/zzyama5hozmr8y7yoj5b.webp`,
   },
   {
     club: `SDG Campus Club`,
     rulebook: `https://drive.google.com/open?id=1R7AXWup3iUB16Cy79zi1UMsaU0gwoaG1`,
     name: `SustainQuest`,
-    description: `"SustainQuest is a high-energy gameshow blending pop culture, movies,
+    description: `SustainQuest is a high-energy gameshow blending pop culture, movies,
 sports, and trending topics with sustainability and SDGs. Featuring rapid-fire
 rounds, buzzers, visuals, and surprise twists, it’s fast, fun, and
-competitive—where knowledge, speed, and strategy collide."`,
+competitive—where knowledge, speed, and strategy collide.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909407/nitrutsav-2026/xlefch37enltethtznsr.webp`,
   },
   {
     club: `Inquizzitive`,
     rulebook: `https://drive.google.com/open?id=15JSad8aXsjG6kyT2to3Cda-XCZPgF7HA`,
     name: `Geeral  Quiz`,
-    description: `"Inquizzitive, the official quiz club of NIT Rourkela, proudly
+    description: `Inquizzitive, the official quiz club of NIT Rourkela, proudly
 presents three flagship quizzes at NITRUTSAV: MELAS Quiz,
 General Quiz, and India Quiz. Each quiz is theme-based, designed
 to test participants across diverse domains including culture,
 history, entertainment, science, current affairs, and Indian
-heritage."`,
+heritage.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909414/nitrutsav-2026/talcezhuxwpzjyqcrtzk.webp`,
   },
   {
     club: `INQUIZZITIVE`,
     rulebook: `https://drive.google.com/open?id=1aqK_fdvsOW0CInAluasZ9OaDmx2m-Wjq`,
     name: `MELA Quiz`,
-    description: `"MELAS Quiz,
+    description: `MELAS Quiz,
 General Quiz, and India Quiz. Each quiz is theme-based, designed
 to test participants across diverse domains including culture,
 history, entertainment, science, current affairs, and Indian
-heritage."`,
+heritage.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909401/nitrutsav-2026/zbl4sps0mnibqtxvak9c.webp`,
   },
   {
     club: `INQUIZZITIVE`,
     rulebook: `https://drive.google.com/open?id=1GTb2Va_JEItk0_KzdNEv6MRO2AtgY6S6`,
     name: `India Quiz`,
-    description: `"MELAS Quiz,
+    description: `MELAS Quiz,
 General Quiz, and India Quiz. Each quiz is theme-based, designed
 to test participants across diverse domains including culture,
 history, entertainment, science, current affairs, and Indian
-heritage."`,
+heritage.`,
     posterurl: `https://res.cloudinary.com/drf3eatfm/image/upload/v1768909400/nitrutsav-2026/kx93iepg3rs0xqdiagjh.webp`,
   },
   {
