b5476aee4f7ec909d8d233ad30f1e72cbd61f9bd
ayush <ayushatschool5@gmail.com>
2026-01-23 01:14:46 +0530

    refactor: added sponsors updated

--- apps/web/config/sponsors/index.ts
+++ apps/web/config/sponsors/index.ts
@@ -2,37 +2,37 @@ export const sponsors = [
   {
     name: "Gaylords Group",
     href: "https://www.gaylordsgroup.com",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768413842/nitrutsav-2026/d5gqsrm8rezffups6l2c.webp",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769110926/d5gqsrm8rezffups6l2c_stxo1r.webp",
     type: "Major Sponsor",
   },
   {
     name: "HPCL",
     href: "https://www.hindustanpetroleum.com/",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768413835/nitrutsav-2026/bsuciidl6vzylrle3fny.png",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769110945/bsuciidl6vzylrle3fny_i7lcfe.png",
     type: "Platinum Sponsor",
   },
   {
     name: "Hotel Neelam",
     href: "",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768413891/nitrutsav-2026/zz0hja4mitnjngvwpt47.png",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769110964/zz0hja4mitnjngvwpt47_w4dhvy.png",
     type: "Platinum Sponsor",
   },
   {
     name: "SBI",
     href: "https://www.easemytrip.com/",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768661075/nitrutsav-2026/k9tkpghhnuryfdgy50gp.png",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769110981/k9tkpghhnuryfdgy50gp_z4wm2x.png",
     type: "Transaction Partner",
   },
   {
     name: "GAIL (India) Limited",
     href: "https://gailonline.com/",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768413818/nitrutsav-2026/cwc8fvzjcirbwcdl1cgj.png",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769110999/cwc8fvzjcirbwcdl1cgj_hsvvlc.png",
     type: "Energy Partner",
   },
   {
     name: "Ease My Trip",
     href: "https://www.easemytrip.com/",
-    logo: "https://res.cloudinary.com/dz4lwydkp/image/upload/v1768661344/nitrutsav-2026/ndpnhsqaoxhgluo8fkr4.svg",
+    logo: "https://res.cloudinary.com/domaovjhf/image/upload/v1769111014/ndpnhsqaoxhgluo8fkr4_q6yvjt.svg",
     type: "Travel Partner",
   },
 ];
